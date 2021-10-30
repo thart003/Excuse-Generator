b823da104ab73592a88b327c9759f97f92f56b35
Taylor Hart <thart003@ucr.edu>
2021-10-30 13:51:51 +0000

    create excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Taylor from the console!");
+};
+
+let generateExcuse = () => {
+  let who = [
+    "my step-son",
+    "the neighbors Rottweiler",
+    "my little Tiger cub",
+    "her sister-in-law"
+  ];
+  let action = ["desicrated", "annihilated", "pacified", "purged"];
+  let what = [
+    "the million-dollar painting",
+    "a gold watch",
+    "a neighbors cat",
+    "the stagecoach"
+  ];
+  let when = [
+    "half past the hour",
+    "just before the buzzer",
+    "between periods",
+    "after closing time"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
